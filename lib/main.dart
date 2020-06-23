b82406730db451dc84cffe34eaa0ed4cec972b28
danielmarques28 <danielmarques7@hotmail.com>
2020-06-23 11:37:35 -0300

    Add default locale and dateformat

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:worktracker/screens/home/Home.dart';
+import 'package:worktracker/screens/Home.dart';
+import 'package:intl/intl.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 void main() {
+  Intl.defaultLocale = 'pt_BR';
+  initializeDateFormatting();
+  
   runApp(MaterialApp(home: Home()));
 }

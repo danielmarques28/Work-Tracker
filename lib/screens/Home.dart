0fc7514359296b0f75842dfbc16c176ef078aa85
danielmarques28 <danielmarques7@hotmail.com>
2020-07-14 11:47:32 -0300

    Remove ListView and add CustomScrollView

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:worktracker/models/CalendarFile.dart';
-import 'package:worktracker/widgets/home/CarouselDay.dart';
+import 'package:worktracker/widgets/home/RowDayCard.dart';
 import 'package:worktracker/widgets/home/InfoDay.dart';
-import 'package:worktracker/widgets/home/ListCards.dart';
+import 'package:worktracker/widgets/home/ColumnRoutineCard.dart';
 import 'package:worktracker/widgets/home/TopBar.dart';
 import 'package:worktracker/widgets/home/DayGraph.dart';
-import 'package:worktracker/widgets/background.dart';
+import 'package:worktracker/helpers/background.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -16,31 +16,27 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   Map<String, dynamic> calendar;
   bool showCarousel = false;
-  String date = DateTime.now().toString().substring(0, 10);
+  String selectedDate = DateTime.now().toString().substring(0, 10);
+  final GlobalKey<RowDayCardState> _globalKey = GlobalKey<RowDayCardState>();
 
   @override
   void initState() {
     super.initState();
-    CalendarFile().readFile().then((value) {
-      setState(() {
-        calendar = value;
-      });
+    _getCalendarContent();
+  }
+
+  void _getCalendarContent() async {
+    await CalendarFile().readFile().then((value) {
+      setState(() => calendar = value);
     });
   }
 
-  Widget _swapContainer() {
-    return showCarousel ?
-      CarouselDay(callback: (value) {
-        setState(() {
-          date = value;
-        });
-      })
-      :
-      InfoDay(callback: (value) {
-        setState(() {
-          showCarousel = value;
-        });
-      });
+  void _setShowCarousel(bool value) {
+    setState(() => showCarousel = value);
+  }
+
+  void _setSelectedDate(String value) {
+    setState(() => selectedDate = value);
   }
 
   @override
@@ -52,16 +48,34 @@ class _HomeState extends State<Home> {
 
     return Scaffold(
       body: Container(
-        height: MediaQuery.of(context).size.height * 1,
-        width: MediaQuery.of(context).size.width * 1,
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
         decoration: background(),
-        child: ListView(
-          shrinkWrap: true,
-          children: [
-            TopBar(),
-            _swapContainer(),
-            DayGraph(),
-            ListCards()
+        child: CustomScrollView(
+          slivers: [
+            SliverToBoxAdapter(child: TopBar()),
+            SliverToBoxAdapter(
+              child: Visibility(
+                maintainState: true,
+                visible: showCarousel,
+                child: RowDayCard(
+                  key: _globalKey,
+                  updateSelectedDate: (value) => _setSelectedDate(value),
+                  close: () => _setShowCarousel(false)
+                )
+              )
+            ),
+            SliverToBoxAdapter(child: Visibility(
+              visible: !showCarousel,
+              child: InfoDay(
+                close: () {
+                  _setShowCarousel(true);
+                  _globalKey.currentState.startCountdown();
+                }
+              )
+            )),
+            SliverToBoxAdapter(child: DayGraph()),
+            SliverToBoxAdapter(child: ColumnRoutineCard())
           ]
         )
       )

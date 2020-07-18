b2cbae3214852046c8d105153b6cf69667332b55
danielmarques28 <danielmarques7@hotmail.com>
2020-07-18 13:20:28 -0300

    Send calendar file content to DayGraph and ColumnRoutineCard widgets

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:worktracker/helpers/responsive.dart';
 import 'package:worktracker/models/CalendarFile.dart';
+import 'package:worktracker/helpers/responsive.dart';
+import 'package:worktracker/helpers/background.dart';
 import 'package:worktracker/widgets/home/RowDayCard.dart';
 import 'package:worktracker/widgets/home/InfoDay.dart';
 import 'package:worktracker/widgets/home/ColumnRoutineCard.dart';
 import 'package:worktracker/widgets/home/TopBar.dart';
 import 'package:worktracker/widgets/home/DayGraph.dart';
-import 'package:worktracker/helpers/background.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -17,7 +17,7 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   Map<String, dynamic> calendar;
   bool showCarousel = false;
-  String selectedDate = DateTime.now().toString().substring(0, 10);
+  String selectedDate;
   final GlobalKey<RowDayCardState> _globalKey = GlobalKey<RowDayCardState>();
 
   @override
@@ -26,9 +26,28 @@ class _HomeState extends State<Home> {
     _getCalendarContent();
   }
 
-  void _getCalendarContent() async {
-    await CalendarFile().readFile().then((value) {
-      setState(() => calendar = value);
+  void _getCalendarContent() {
+    CalendarFile().readFile().then((value) {
+      setState(() {
+        calendar = value;
+        selectedDate = DateTime.now().toString().substring(0, 10);
+        calendar[selectedDate] = [];
+      });
+      for (int i = 1; i <= 3; i++) {
+        setState(() {
+          calendar[selectedDate].add({'routine_id': i, 'status': 1});
+        });
+      }
+      for (int i = 4; i <= 6; i++) {
+        setState(() {
+          calendar[selectedDate].add({'routine_id': i, 'status': 2});
+        });
+      }
+      for (int i = 7; i <= 9; i++) {
+        setState(() {
+          calendar[selectedDate].add({'routine_id': i, 'status': 3});
+        });
+      }
     });
   }
 
@@ -55,17 +74,15 @@ class _HomeState extends State<Home> {
         child: CustomScrollView(
           slivers: [
             SliverToBoxAdapter(child: TopBar()),
-            SliverToBoxAdapter(
-              child: Visibility(
-                maintainState: true,
-                visible: showCarousel,
-                child: RowDayCard(
-                  key: _globalKey,
-                  updateSelectedDate: (value) => _setSelectedDate(value),
-                  close: () => _setShowCarousel(false)
-                )
+            SliverToBoxAdapter(child: Visibility(
+              maintainState: true,
+              visible: showCarousel,
+              child: RowDayCard(
+                key: _globalKey,
+                updateSelectedDate: (value) => _setSelectedDate(value),
+                close: () => _setShowCarousel(false)
               )
-            ),
+            )),
             SliverToBoxAdapter(child: Visibility(
               visible: !showCarousel,
               child: InfoDay(
@@ -75,8 +92,16 @@ class _HomeState extends State<Home> {
                 }
               )
             )),
-            SliverToBoxAdapter(child: DayGraph()),
-            SliverToBoxAdapter(child: ColumnRoutineCard())
+            SliverToBoxAdapter(
+              child: calendar != null
+                ? DayGraph(dayRoutines: calendar[selectedDate])
+                : Container()
+            ),
+            SliverToBoxAdapter(
+              child: calendar != null
+                ? ColumnRoutineCard(dayRoutines: calendar[selectedDate])
+                : Container()
+            )
           ]
         )
       )

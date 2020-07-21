99d57bcacbdca274ad660ea2850e0b850cb7c295
danielmarques28 <danielmarques7@hotmail.com>
2020-07-21 02:36:43 -0300

    Remove snackbar and sort list of routine cards based on routine status

--- lib/widgets/home/ColumnRoutineCard.dart
+++ lib/widgets/home/ColumnRoutineCard.dart
@@ -16,17 +16,17 @@ class Background extends StatelessWidget {
 
   List<Widget> _swapRowChildrens(BuildContext context) {
     Widget circleIcon = Container(
-      width: deviceWidth(context, 0.13),
-      height: deviceHeigth(context, 0.13),
+      width: deviceWidth(context, 0.11),
+      height: deviceHeigth(context, 0.11),
       margin: EdgeInsets.only(
-        left: deviceWidth(context, 0.02),
-        right: deviceWidth(context, 0.02)
+        left: deviceWidth(context, 0.01),
+        right: deviceWidth(context, 0.01)
       ),
       decoration: BoxDecoration(
         color: color,
         shape: BoxShape.circle
       ),
-      child: Icon(icon, color: Colors.white, size: 35.0)
+      child: Icon(icon, color: Colors.white, size: 30.0)
     );
     Widget arrowIcon = Transform.rotate(
       angle: direction == DismissDirection.startToEnd ? pi : pi * 2,
@@ -34,7 +34,7 @@ class Background extends StatelessWidget {
         child: Icon(
           Icons.arrow_back_ios,
           color: Colors.white,
-          size: 26.0
+          size: 23.0
         )
       )
     );
@@ -55,7 +55,7 @@ class Background extends StatelessWidget {
       alignment: direction == DismissDirection.startToEnd ?
           Alignment.centerLeft : Alignment.centerRight,
       child: Container(
-        width: deviceWidth(context, 0.26),
+        width: deviceWidth(context, 0.19),
         child: Row(children: _swapRowChildrens(context))
       )
     );
@@ -65,128 +65,133 @@ class Background extends StatelessWidget {
 class ColumnRoutineCard extends StatefulWidget {
   ColumnRoutineCard({
     Key key,
-    @required this.dayRoutines
+    @required this.dayRoutines,
+    @required this.updateFile
   }) : super(key: key);
 
   final List<dynamic> dayRoutines;
+  final Function updateFile;
 
   @override
-  _ColumnRoutineCardState createState() => _ColumnRoutineCardState();
+  ColumnRoutineCardState createState() => ColumnRoutineCardState();
 }
 
-class _ColumnRoutineCardState extends State<ColumnRoutineCard> {
-  List<RoutineCard> _listCards;
+class ColumnRoutineCardState extends State<ColumnRoutineCard> {
   final GlobalKey<AnimatedListState> _listGlobalKey
     = GlobalKey<AnimatedListState>();
-  List _dayRoutines;
+  List<dynamic> _dayRoutines;
 
   @override
   void initState() {
     super.initState();
-    setState(() => _dayRoutines = widget.dayRoutines);
     sortDayRoutines();
-    generateListCards();
   }
 
   sortDayRoutines() {
     setState(() {
-      _dayRoutines.sort((a, b) => a['status'].compareTo(b['status']));
+      _dayRoutines = widget.dayRoutines;
+      _dayRoutines.sort((a, b) {
+        return a['status'].compareTo(b['status']);
+      });
     });
   }
 
-  generateListCards() {
-    setState(() {
-      _listCards = List.generate(
-        _dayRoutines.length,
-        (index) => RoutineCard(
-          routine: _dayRoutines[index]
-        )
-      );
-    });
-  }
-
-  SnackBar getSnackBar(RoutineCard deletedCard, int index) {
-    final Text content = Text(
-      '"Rotina ${_dayRoutines[index]['routine_id']}" não realizada',
-      style: TextStyle(
-        fontWeight: FontWeight.bold,
-        color: Colors.white
-      )
-    );
-
-    final Material actionButton = Material(
-      color: Colors.transparent,
-      child: InkWell(
-        borderRadius: BorderRadius.circular(7.0),
-        onTap: () {
-          setState(() {
-            _listCards.insert(index, deletedCard);
-          });
-          _listGlobalKey.currentState.insertItem(index);
-          Scaffold.of(context).hideCurrentSnackBar();
-        },
-        child: Container(
-          padding: EdgeInsets.all(8.0),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(7.0)
-          ),
-          child: Text(
-            'Desfazer',
-            style: TextStyle(color: Colors.white)
-          )
-        )
-      )
-    );
-
-    return SnackBar(
-      behavior: SnackBarBehavior.floating,
-      duration: Duration(seconds: 3),
-      content: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          content,
-          actionButton
-        ]
-      )
-    );
-  }
-
-  decideDirection(int status) {
+  DismissDirection _decideDirection(int status) {
+    DismissDirection direction;
     switch (status) {
       case 1:
-        return DismissDirection.horizontal;
+        direction = DismissDirection.horizontal;
+        break;
       case 2:
-        return DismissDirection.endToStart;
+        direction = DismissDirection.endToStart;
+        break;
       case 3:
-        return DismissDirection.startToEnd;
+        direction = DismissDirection.startToEnd;
+        break;
     }
+    return direction;
   }
 
-  RoutineCard deleteCard(DismissDirection direction, int index) {
-    RoutineCard deletedCard;
+  _removeItem(int removeIndex) {
+    Map routine;
     setState(() {
-      deletedCard = _listCards.removeAt(index);
+      routine = _dayRoutines.removeAt(removeIndex);
     });
-    _listGlobalKey.currentState
-      .removeItem(
-        index, (context, animation) => Container()
-      );
-    // Scaffold.of(context)
-    //   .showSnackBar(getSnackBar(deletedCard, index));
-    return deletedCard;
+    _listGlobalKey.currentState.removeItem(
+      removeIndex, (context, animation) =>  Container()
+    );
+
+    return routine;
   }
 
-  void insertCard(DismissDirection direction, RoutineCard deletedCard) {
-    final int statusTarget =
-      direction == DismissDirection.startToEnd ? 2 : 3;
-    final int insertIndex = _dayRoutines.indexWhere(
+  void _insertItem(DismissDirection direction, Map newItem) {
+    int statusTarget;
+    if(direction == DismissDirection.startToEnd) {
+      statusTarget = 2;
+      newItem['status'] = 2;
+    }
+    else {
+      statusTarget = 3;
+      newItem['status'] = 3;
+    }
+
+    int insertIndex = _dayRoutines.indexWhere(
       (element) => element['status'] == statusTarget
-    ) - 1;
+    );
+
+    if(insertIndex == -1) {
+      if(statusTarget == 2) {
+        insertIndex = _dayRoutines.lastIndexWhere(
+          (element) => element['status'] == 1
+        );
+        insertIndex += 1;
+      }
+      else if(statusTarget == 3)
+        insertIndex = _dayRoutines.length;
+    }
+
     setState(() {
-      _listCards.insert(insertIndex, deletedCard);
-      _listGlobalKey.currentState
-        .insertItem(insertIndex);
+      _dayRoutines.insert(insertIndex, newItem);
+      _listGlobalKey.currentState.insertItem(insertIndex);
     });
+    widget.updateFile?.call(_dayRoutines);
+  }
+
+  Widget _buildDismissible(routine, index, animation) {
+    return Dismissible(
+      key: UniqueKey(),
+      background: Background(
+        color: Colors.blueAccent,
+        icon: Icons.check,
+        direction: DismissDirection.startToEnd
+      ),
+      secondaryBackground: Background(
+        color: Colors.redAccent,
+        icon: Icons.close,
+        direction: DismissDirection.endToStart
+      ),
+      direction:
+        _decideDirection(routine['status']),
+      onDismissed: (direction) {
+        Map removedItem = _removeItem(index);
+        _insertItem(direction, removedItem);
+      },
+      child: SizeTransition(
+        axis: Axis.vertical,
+        sizeFactor: animation,
+        child: RoutineCard(routine: routine)
+      )
+    );
+  }
+
+  Widget _buildItem(routine, index, animation) {
+    return Container(
+      alignment: Alignment.center,
+      margin: EdgeInsets.only(
+        bottom: deviceHeigth(context, 0.0125)
+      ),
+      child: _buildDismissible(routine, index, animation)
+    );
   }
 
   @override
@@ -199,47 +204,17 @@ class _ColumnRoutineCardState extends State<ColumnRoutineCard> {
         bottom: deviceHeigth(context, 0.018)
       ),
       alignment: Alignment.center,
-      child: AnimatedList(
-        physics: NeverScrollableScrollPhysics(),
-        shrinkWrap: true,
-        key: _listGlobalKey,
-        initialItemCount: _listCards.length,
-        itemBuilder: (context, index, animation) {
-          return Container(
-            alignment: Alignment.center,
-            margin: EdgeInsets.only(
-              bottom: deviceHeigth(context, 0.0125)
-            ),
-            child: Center(
-              child: Dismissible(
-                key: Key('${_listCards[index]}'),
-                background: Background(
-                  color: Colors.green,
-                  icon: Icons.check,
-                  direction: DismissDirection.startToEnd
-                ),
-                secondaryBackground: Background(
-                  color: Colors.red,
-                  icon: Icons.close,
-                  direction: DismissDirection.endToStart
-                ),
-                direction:
-                  decideDirection(_dayRoutines[index]['status']),
-                onDismissed: (direction) {
-                  RoutineCard deletedCard
-                    = deleteCard(direction, index);
-                  insertCard(direction, deletedCard);
-                },
-                child: SizeTransition(
-                  axis: Axis.vertical,
-                  sizeFactor: animation,
-                  child: _listCards[index]
-                )
-              )
-            )
-          );
-        }
-      )
+      child: _dayRoutines.length > 0
+        ? AnimatedList(
+            physics: NeverScrollableScrollPhysics(),
+            shrinkWrap: true,
+            key: _listGlobalKey,
+            initialItemCount: _dayRoutines.length,
+            itemBuilder: (context, index, animation) {
+              return _buildItem(_dayRoutines[index], index, animation);
+            }
+          )
+        : Container()
     );
   }
 }

64e442977ed7eaa369f5fd4afe8e5f9cbc19fa85
danielmarques28 <danielmarques7@hotmail.com>
2020-07-21 02:32:03 -0300

    Add background color depending of routine status

--- lib/widgets/home/RoutineCard.dart
+++ lib/widgets/home/RoutineCard.dart
@@ -1,4 +1,3 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:worktracker/helpers/responsive.dart';
 
@@ -12,7 +11,21 @@ class RoutineCard extends StatefulWidget {
 }
 
 class _RoutineCardState extends State<RoutineCard> {
-  bool _getBigger = false;
+  Color _decideCardColor() {
+    Color color;
+    switch (widget.routine['status']) {
+      case 1:
+        color = Colors.white;
+        break;
+      case 2:
+        color = Colors.blueAccent;
+        break;
+      case 3:
+        color = Colors.redAccent;
+        break;
+    }
+    return color.withOpacity(0.24);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,18 +33,13 @@ class _RoutineCardState extends State<RoutineCard> {
       color: Colors.transparent,
       child: InkWell(
         onLongPress: () {
-          setState(() => _getBigger = true);
-          Timer(
-            Duration(seconds: 5),
-            () => setState(() => _getBigger = false)
-          );
+          print('routine card long press');
         },
-        child: AnimatedContainer(
-          duration: Duration(milliseconds: 300),
+        child: Container(
           width: deviceWidth(context, 0.9),
-          height: deviceHeigth(context, _getBigger ? 0.2 : 0.12),
+          height: deviceHeigth(context, 0.12),
           decoration: BoxDecoration(
-            color: Colors.white24,
+            color: _decideCardColor(),
             borderRadius: BorderRadius.circular(7.0)
           ),
           child: Container(
@@ -48,8 +56,7 @@ class _RoutineCardState extends State<RoutineCard> {
                 Align(
                   alignment: Alignment.topLeft,
                   child: Text(
-                    'Rotina ${widget.routine['routine_id']} - ${widget.routine['status']}'
-                      .toUpperCase(),
+                    'Rotina ${widget.routine['routine_id']}'.toUpperCase(),
                     textAlign: TextAlign.left,
                     style: TextStyle(
                       color: Colors.white,

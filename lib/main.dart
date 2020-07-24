7da7abb5af290d5fab021298132a413e3bf6fd7f
danielmarques28 <danielmarques7@hotmail.com>
2020-07-24 10:25:45 -0300

    Add settings, create routine routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:worktracker/screens/CreateRoutine.dart';
 import 'package:worktracker/screens/History.dart';
+import 'package:worktracker/screens/Settings.dart';
 import 'package:worktracker/screens/Home.dart';
 
 void main() {
@@ -12,7 +14,9 @@ void main() {
     initialRoute: '/',
     routes: {
       '/': (context) => Home(),
-      '/history': (context) => History()
+      '/history': (context) => History(),
+      '/settings': (context) => Settings(),
+      '/create-routine': (context) => CreateRoutine()
     }
   ));
 }
